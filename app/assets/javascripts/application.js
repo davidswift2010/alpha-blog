dddc430c32ae4a25bef27ccc2ce6585ea00ebbca
DSwifty <davidswift2010@yahoo.com>
2019-01-18 19:44:35 -0600

    changed application.js require bootstrap sprockets to bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
